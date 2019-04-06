c49208316888543cf0ac21ffc84c0e3b8a9798f7
Brooke Nesoi <bnesoi@mi-f6.21-school.ru>
2019-04-06 15:24:19 +0300

    memset bzero

--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/06 15:17:05 by bnesoi            #+#    #+#             */
+/*   Updated: 2019/04/06 15:17:20 by bnesoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -5,7 +17,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-void			*ft_memset(void *str, int c, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
--- main.c
+++ main.c
@@ -1,6 +1,52 @@
-#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/06 14:56:41 by bnesoi            #+#    #+#             */
+/*   Updated: 2019/04/06 15:23:49 by bnesoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(void)
+#include "libft.h"
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
+void	test_bzero(void)
 {
+	char s0[5] = "....";
+	char s1[5] = ",,,,";
+	char s2[1] = "";
+	char s3[1] = "";
+
+	ft_bzero(s0, 4);
+	bzero(s1, 4);
+	ft_bzero(s2, 4);
+	bzero(s3, 4);
+	int i = 5;
+	while (i--)
+	{
+		assert(s0[i] == s1[i]);
+	}
+}
+
+void	test_memset(void)
+{
+	char s0[5] = "....";
+	char s1[5] = "....";
+	char s2[1] = "";
+	char s3[1] = "";
+
+	assert(strcmp(ft_memset(s0, 'z', 4), memset(s1, 'z', 4)) == 0);
+	assert(strcmp(ft_memset(s2, 'z', 0), memset(s3, 'z', 0)) == 0);
+}
+
+int		main(void)
+{
+	test_bzero();
+	test_memset();
 	return (0);
 }
