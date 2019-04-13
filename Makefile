885806b3e4ae14ef5f62bcb58bec8df678dd56c0
Brooke Nesoi <bnesoi@at-l2.21-school.ru>
2019-04-13 14:03:06 +0300

    norm2

--- Makefile
+++ Makefile
@@ -3,60 +3,60 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: bnesoi <marvin@42.fr>                      +#+  +:+       +#+         #
+#    By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/11 15:11:22 by bnesoi            #+#    #+#              #
-#    Updated: 2019/03/12 18:58:05 by bnesoi           ###   ########.fr        #
+#    Updated: 2019/04/13 13:52:36 by bnesoi           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME=libft.a
 SRCS=\
-    ft_lstnew.c        ft_queue_new.c   ft_strmapi.c     ft_strhash.c          \
-    ft_lstreverse.c    ft_stack_del.c   ft_strncat.c                           \
-    ft_atoi.c          ft_memalloc.c    ft_stack_new.c   ft_strncmp.c          \
-    ft_bzero.c         ft_memccpy.c     ft_stack_pop.c   ft_strncpy.c          \
-    ft_count_till.c    ft_memchr.c      ft_stack_push.c  ft_strnequ.c          \
-    ft_dequeue.c       ft_memcmp.c      ft_strcat.c      ft_strnew.c           \
-    ft_enqueue.c       ft_memcpy.c      ft_strchr.c      ft_strnstr.c          \
-    ft_isalnum.c       ft_memdel.c      ft_strclr.c      ft_strrchr.c          \
-    ft_isalpha.c       ft_memmove.c     ft_strcmp.c      ft_strsplit.c         \
-    ft_isascii.c       ft_memset.c      ft_strcpy.c      ft_strstr.c           \
-    ft_isdigit.c       ft_putchar.c     ft_strdel.c      ft_strsub.c           \
-    ft_isprint.c       ft_putchar_fd.c  ft_strdup.c      ft_strtrim.c          \
-    ft_iswhitespace.c  ft_putendl.c     ft_strequ.c      ft_tolower.c          \
-    ft_itoa.c          ft_putendl_fd.c  ft_striter.c     ft_toupper.c          \
-    ft_lstadd.c        ft_putnbr.c      ft_striteri.c    ft_power.c            \
-    ft_lstdel.c        ft_putnbr_fd.c   ft_strjoin.c     ft_lstfind.c          \
-    ft_lstdelone.c     ft_putstr.c      ft_strlcat.c     ft_htnew.c            \
-    ft_lstiter.c       ft_putstr_fd.c   ft_strlen.c      ft_itoa_base.c        \
-    ft_lstmap.c        ft_queue_del.c   ft_strmap.c      ft_print_bits_fd.c    \
-    ft_htdel.c         ft_htget.c       ft_htset.c       ft_itoa_base_ext.c    \
-    ft_tap_ok.c        ft_tap_is.c      ft_tap_ismem.c   ft_strlowcase.c       \
-    ft_strcapitalize.c
+	ft_lstnew.c        ft_queue_new.c   ft_strmapi.c     ft_strhash.c          \
+	ft_lstreverse.c    ft_stack_del.c   ft_strncat.c                           \
+	ft_atoi.c          ft_memalloc.c    ft_stack_new.c   ft_strncmp.c          \
+	ft_bzero.c         ft_memccpy.c     ft_stack_pop.c   ft_strncpy.c          \
+	ft_count_till.c    ft_memchr.c      ft_stack_push.c  ft_strnequ.c          \
+	ft_dequeue.c       ft_memcmp.c      ft_strcat.c      ft_strnew.c           \
+	ft_enqueue.c       ft_memcpy.c      ft_strchr.c      ft_strnstr.c          \
+	ft_isalnum.c       ft_memdel.c      ft_strclr.c      ft_strrchr.c          \
+	ft_isalpha.c       ft_memmove.c     ft_strcmp.c      ft_strsplit.c         \
+	ft_isascii.c       ft_memset.c      ft_strcpy.c      ft_strstr.c           \
+	ft_isdigit.c       ft_putchar.c     ft_strdel.c      ft_strsub.c           \
+	ft_isprint.c       ft_putchar_fd.c  ft_strdup.c      ft_strtrim.c          \
+	ft_iswhitespace.c  ft_putendl.c     ft_strequ.c      ft_tolower.c          \
+	ft_itoa.c          ft_putendl_fd.c  ft_striter.c     ft_toupper.c          \
+	ft_lstadd.c        ft_putnbr.c      ft_striteri.c    ft_power.c            \
+	ft_lstdel.c        ft_putnbr_fd.c   ft_strjoin.c     ft_lstfind.c          \
+	ft_lstdelone.c     ft_putstr.c      ft_strlcat.c     ft_htnew.c            \
+	ft_lstiter.c       ft_putstr_fd.c   ft_strlen.c      ft_itoa_base.c        \
+	ft_lstmap.c        ft_queue_del.c   ft_strmap.c      ft_print_bits_fd.c    \
+	ft_htdel.c         ft_htget.c       ft_htset.c       ft_itoa_base_ext.c    \
+														 ft_strlowcase.c       \
+	ft_strcapitalize.c
 OBJECT_FILES=\
-    ft_lstnew.o        ft_queue_new.o   ft_strmapi.o     ft_strhash.o          \
-    ft_lstreverse.o    ft_stack_del.o   ft_strncat.o                           \
-    ft_atoi.o          ft_memalloc.o    ft_stack_new.o   ft_strncmp.o          \
-    ft_bzero.o         ft_memccpy.o     ft_stack_pop.o   ft_strncpy.o          \
-    ft_count_till.o    ft_memchr.o      ft_stack_push.o  ft_strnequ.o          \
-    ft_dequeue.o       ft_memcmp.o      ft_strcat.o      ft_strnew.o           \
-    ft_enqueue.o       ft_memcpy.o      ft_strchr.o      ft_strnstr.o          \
-    ft_isalnum.o       ft_memdel.o      ft_strclr.o      ft_strrchr.o          \
-    ft_isalpha.o       ft_memmove.o     ft_strcmp.o      ft_strsplit.o         \
-    ft_isascii.o       ft_memset.o      ft_strcpy.o      ft_strstr.o           \
-    ft_isdigit.o       ft_putchar.o     ft_strdel.o      ft_strsub.o           \
-    ft_isprint.o       ft_putchar_fd.o  ft_strdup.o      ft_strtrim.o          \
-    ft_iswhitespace.o  ft_putendl.o     ft_strequ.o      ft_tolower.o          \
-    ft_itoa.o          ft_putendl_fd.o  ft_striter.o     ft_toupper.o          \
-    ft_lstadd.o        ft_putnbr.o      ft_striteri.o    ft_power.o            \
-    ft_lstdel.o        ft_putnbr_fd.o   ft_strjoin.o     ft_lstfind.o          \
-    ft_lstdelone.o     ft_putstr.o      ft_strlcat.o     ft_htnew.o            \
-    ft_lstiter.o       ft_putstr_fd.o   ft_strlen.o      ft_itoa_base.o        \
-    ft_lstmap.o        ft_queue_del.o   ft_strmap.o      ft_print_bits_fd.o    \
-    ft_htdel.o         ft_htget.o       ft_htset.o       ft_itoa_base_ext.o    \
-    ft_tap_ok.o        ft_tap_is.o      ft_tap_ismem.o   ft_strlowcase.o       \
-    ft_strcapitalize.o
+	ft_lstnew.o        ft_queue_new.o   ft_strmapi.o     ft_strhash.o          \
+	ft_lstreverse.o    ft_stack_del.o   ft_strncat.o                           \
+	ft_atoi.o          ft_memalloc.o    ft_stack_new.o   ft_strncmp.o          \
+	ft_bzero.o         ft_memccpy.o     ft_stack_pop.o   ft_strncpy.o          \
+	ft_count_till.o    ft_memchr.o      ft_stack_push.o  ft_strnequ.o          \
+	ft_dequeue.o       ft_memcmp.o      ft_strcat.o      ft_strnew.o           \
+	ft_enqueue.o       ft_memcpy.o      ft_strchr.o      ft_strnstr.o          \
+	ft_isalnum.o       ft_memdel.o      ft_strclr.o      ft_strrchr.o          \
+	ft_isalpha.o       ft_memmove.o     ft_strcmp.o      ft_strsplit.o         \
+	ft_isascii.o       ft_memset.o      ft_strcpy.o      ft_strstr.o           \
+	ft_isdigit.o       ft_putchar.o     ft_strdel.o      ft_strsub.o           \
+	ft_isprint.o       ft_putchar_fd.o  ft_strdup.o      ft_strtrim.o          \
+	ft_iswhitespace.o  ft_putendl.o     ft_strequ.o      ft_tolower.o          \
+	ft_itoa.o          ft_putendl_fd.o  ft_striter.o     ft_toupper.o          \
+	ft_lstadd.o        ft_putnbr.o      ft_striteri.o    ft_power.o            \
+	ft_lstdel.o        ft_putnbr_fd.o   ft_strjoin.o     ft_lstfind.o          \
+	ft_lstdelone.o     ft_putstr.o      ft_strlcat.o     ft_htnew.o            \
+	ft_lstiter.o       ft_putstr_fd.o   ft_strlen.o      ft_itoa_base.o        \
+	ft_lstmap.o        ft_queue_del.o   ft_strmap.o      ft_print_bits_fd.o    \
+	ft_htdel.o         ft_htget.o       ft_htset.o       ft_itoa_base_ext.o    \
+														 ft_strlowcase.o       \
+	ft_strcapitalize.o
 GCC_FLAGS=-Wall -Wextra -Werror
 all : $(NAME)
 
--- tests.c
+++ tests.c
@@ -1,71 +1,145 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   tests.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 14:56:41 by bnesoi            #+#    #+#             */
-/*   Updated: 2019/04/06 15:55:31 by bnesoi           ###   ########.fr       */
+/*   Updated: 2019/04/13 13:58:54 by bnesoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+void	ft_tap_ismem(const char *file, int line,
+					 const char *a, const char *b, size_t n, const char *name, ...)
+{
+	size_t	i;
+
+	i = 0;
+	while (i++ < n)
+	{
+		if (*a != *b)
+		{
+			ft_putstr("KO\n");
+			ft_putstr("#   Failed ");
+			name ? ft_putstr(name) : ft_putstr("test");
+			!name ? ft_putstr(" ") : ft_putstr("\n#   ");
+			ft_putstr("at ");
+			ft_putstr(file);
+			ft_putstr(" line ");
+			ft_putnbr(line);
+			ft_putstr("\n#      diff at: a[");
+			ft_putnbr(i-1);
+			ft_putstr("]\n");
+			return ;
+		}
+		a++;
+		b++;
+	}
+	ft_putstr("OK\n");
+}
+
+void	ft_tap_is(const char *file, int line,
+				  const char *a, const char *b, const char *name, ...)
+{
+	if (ft_strcmp(a, b))
+	{
+		ft_putstr("KO\n");
+		ft_putstr("#   Failed ");
+		name ? ft_putstr(name) : ft_putstr("test");
+		!name ? ft_putstr(" ") : ft_putstr("\n#   ");
+		ft_putstr("at ");
+		ft_putstr(file);
+		ft_putstr(" line ");
+		ft_putnbr(line);
+		ft_putstr("\n#          got: '");
+		ft_putstr(a);
+		ft_putstr("'\n#     expected: '");
+		ft_putstr(b);
+		ft_putstr("'\n");
+	}
+	else
+		ft_putstr("OK\n");
+}
+
+void	ft_tap_ok(const char *file, int line,
+				  int a, const char *name, ...)
+{
+	if (!a)
+	{
+		ft_putstr("KO\n");
+		ft_putstr("#   Failed ");
+		name ? ft_putstr(name) : ft_putstr("test");
+		!name ? ft_putstr(" ") : ft_putstr("\n#   ");
+		ft_putstr("at ");
+		ft_putstr(file);
+		ft_putstr(" line ");
+		ft_putnbr(line);
+		ft_putstr("\n");
+	}
+	else
+		ft_putstr("OK\n");
+}
+
+#define is(...)(ft_tap_is(__FILE__, __LINE__, __VA_ARGS__, NULL))
+#define ok(...)(ft_tap_ok(__FILE__, __LINE__, __VA_ARGS__, NULL))
+#define ismem(...)(ft_tap_ismem(__FILE__, __LINE__, __VA_ARGS__, NULL))
 
 void	test_ft_memset(void)
 {
 #if defined(FT_MEMSET) || defined(FT_ALL) || defined(FT_PART1)
-    char s0[5] = "....";
-    char s1[5] = "....";
-    is(ft_memset(s0, 'z', 4), memset(s1, 'z', 4));
-    char s2[2] = "1";
-    char s3[2] = "1";
-    is(ft_memset(s2, 'z', 1), memset(s3, 'z', 1));
-    // dies_ok({ft_memset("", 'z', -1);});
+	char s0[5] = "....";
+	char s1[5] = "....";
+	is(ft_memset(s0, 'z', 4), memset(s1, 'z', 4));
+	char s2[2] = "1";
+	char s3[2] = "1";
+	is(ft_memset(s2, 'z', 1), memset(s3, 'z', 1));
 #endif
 }
+
 void	test_ft_bzero(void)
 {
 #if defined(FT_BZERO) || defined(FT_ALL) || defined(FT_PART1)
-    char s0[5] = "....";
-    ft_bzero(s0, 5);
+	char s0[5] = "....";
+	ft_bzero(s0, 5);
 	ismem(s0, "\0\0\0\0\0", 5, "complete bzero");
-    // dies_ok({ft_bzero("", -1);});
+	// dies_ok({ft_bzero("", -1);});
 #endif
 }
 void	test_ft_memcpy(void)
 {
 #if defined(FT_MEMCPY) || defined(FT_ALL) || defined(FT_PART1)
-    char d0[5] = "....";
-    ft_memcpy(d0, "____", 5);
+	char d0[5] = "....";
+	ft_memcpy(d0, "____", 5);
 	ismem(d0, "____", 5);
-    char d1[1] = "";
-    ft_memcpy(d0, "____", 1);
+	char d1[1] = "";
+	ft_memcpy(d0, "____", 1);
 	ismem(d1, "", 1);
-    // dies_ok({ft_memcpy("", "", -1);});
+	// dies_ok({ft_memcpy("", "", -1);});
 #endif
 }
 void	test_ft_memccpy(void)
 {
 #if defined(FT_MEMCCPY) || defined(FT_ALL) || defined(FT_PART1)
-    char d0[5] = "..!.";
-    ft_memccpy(d0, "__!_", '!', 5);
-    is(d0, "__!.");
-    ft_memccpy(d0, "__!_", '_', 5);
-    is(d0, "__!.");
-    ft_memccpy(d0, "\0__!", '!', 5);
+	char d0[5] = "..!.";
+	ft_memccpy(d0, "__!_", '!', 5);
+	is(d0, "__!.");
+	ft_memccpy(d0, "__!_", '_', 5);
+	is(d0, "__!.");
+	ft_memccpy(d0, "\0__!", '!', 5);
 	ismem(d0, "\0__!", 5);
-    // dies_ok({ft_memccpy("", "", 'd', -1);});
+	// dies_ok({ft_memccpy("", "", 'd', -1);});
 #endif
 }
 void	test_ft_memmove(void)
 {
 #if defined(FT_MEMMOVE) || defined(FT_ALL) || defined(FT_PART1)
-    char    a[] = "lorem ipsum dolor sit amet";
-    char	*d;
-    d = a + 1;
-    ft_memmove(d, a, 4);
-    is(d, "lore ipsum dolor sit amet");
+	char    a[] = "lorem ipsum dolor sit amet";
+	char	*d;
+	d = a + 1;
+	ft_memmove(d, a, 4);
+	is(d, "lore ipsum dolor sit amet");
 #endif
 }
 void	test_ft_memchr(void)
@@ -266,25 +340,25 @@ void	test_ft_itoa(void)
 void	test_ft_itoa_base(void)
 {
 #if defined(FT_ITOA_BASE) || defined(FT_ALL) || defined(FT_EXTRA)
-    char d[5] = "abcd";
-    char c[1] = "!";
-    char *p = NULL;
-    is(ft_itoa_base_ext(d, 128, 10, 1), "128");
-    is(ft_itoa_base_ext(d, 11, 10, 1), "11");
-    is(ft_itoa_base_ext(d, 1, 10, 1), "1");
-    is(ft_itoa_base_ext(d, 0, 10, 1), "0");
-    is(ft_itoa_base_ext(d, -1, 10, 1), "-1");
-    is(ft_itoa_base_ext(d, -12, 10, 1), "-12");
+	char d[5] = "abcd";
+	char c[1] = "!";
+	char *p = NULL;
+	is(ft_itoa_base_ext(d, 128, 10, 1), "128");
+	is(ft_itoa_base_ext(d, 11, 10, 1), "11");
+	is(ft_itoa_base_ext(d, 1, 10, 1), "1");
+	is(ft_itoa_base_ext(d, 0, 10, 1), "0");
+	is(ft_itoa_base_ext(d, -1, 10, 1), "-1");
+	is(ft_itoa_base_ext(d, -12, 10, 1), "-12");
 
-    is(ft_itoa_base_ext(d, 1, 2, 1), "1");
-    is(ft_itoa_base_ext(d, 0, 2, 1), "0");
+	is(ft_itoa_base_ext(d, 1, 2, 1), "1");
+	is(ft_itoa_base_ext(d, 0, 2, 1), "0");
 
-    is(ft_itoa_base_ext(d, 4096, 16, 1), "1000");
-    is(ft_itoa_base_ext(d, 128, 16, 1), "80");
-    is(ft_itoa_base_ext(d, 10, 16, 1), "A");
-    is((p = ft_itoa_base(4096, 16)), "1000");
-    ok(c[0] == '!');
-    free(p);
+	is(ft_itoa_base_ext(d, 4096, 16, 1), "1000");
+	is(ft_itoa_base_ext(d, 128, 16, 1), "80");
+	is(ft_itoa_base_ext(d, 10, 16, 1), "A");
+	is((p = ft_itoa_base(4096, 16)), "1000");
+	ok(c[0] == '!');
+	free(p);
 #endif
 }
 void	test_ft_putchar(void)
@@ -365,142 +439,142 @@ void	test_ft_count_till(void)
 void	test_ft_iswhitespace(void)
 {
 #if defined(FT_ISWHITESPACE) || defined(FT_ALL) || defined(FT_EXTRA2)
-    ok(ft_iswhitespace('\t'));
-    ok(ft_iswhitespace(' '));
-    ok(ft_iswhitespace('\n'));
-    ok(!ft_iswhitespace('\0'));
-    ok(!ft_iswhitespace('1'));
-    ok(!ft_iswhitespace('a'));
+	ok(ft_iswhitespace('\t'));
+	ok(ft_iswhitespace(' '));
+	ok(ft_iswhitespace('\n'));
+	ok(!ft_iswhitespace('\0'));
+	ok(!ft_iswhitespace('1'));
+	ok(!ft_iswhitespace('a'));
 #endif
 }
 void	test_ft_lstreverse(void)
 {
 #if defined(FT_LSTREVERSE) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_list *l;
-    t_list c = {"c", 2, NULL};
-    t_list b = {"b", 2, &c};
-    t_list a = {"a", 2, &b};
-    l = &a;
-    ft_lstreverse(&l);
-    is(l->content, "c");
-    is(l->next->content, "b");
-    is(l->next->next->content, "a");
-    // lives_ok({ft_lstreverse(NULL);});
+	t_list *l;
+	t_list c = {"c", 2, NULL};
+	t_list b = {"b", 2, &c};
+	t_list a = {"a", 2, &b};
+	l = &a;
+	ft_lstreverse(&l);
+	is(l->content, "c");
+	is(l->next->content, "b");
+	is(l->next->next->content, "a");
+	// lives_ok({ft_lstreverse(NULL);});
 #endif
 }
 void	test_ft_stack_new(void)
 {
 #if defined(FT_STACK_NEW) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_stack *s = ft_stack_new();
-    ok(!s->top);
-    free(s);
+	t_stack *s = ft_stack_new();
+	ok(!s->top);
+	free(s);
 #endif
 }
 void    stack_del(void *c, size_t s)
 {
-    memset(c, '!', s);
-    (void)c;
-    (void)s;
+	memset(c, '!', s);
+	(void)c;
+	(void)s;
 }
 void	test_ft_stack_del(void)
 {
 #if defined(FT_STACK_DEL) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_stack *s = malloc(sizeof(t_stack));
-    t_list  *l = malloc(sizeof(t_list));
-    char c[] = ".....";
-    *l = (t_list){c, 4, NULL};
-    s->top = l;
-    ft_stack_del(&s, stack_del);
-    ok(s == NULL);
-    is(c, "!!!!.");
+	t_stack *s = malloc(sizeof(t_stack));
+	t_list  *l = malloc(sizeof(t_list));
+	char c[] = ".....";
+	*l = (t_list){c, 4, NULL};
+	s->top = l;
+	ft_stack_del(&s, stack_del);
+	ok(s == NULL);
+	is(c, "!!!!.");
 #endif
 }
 void	test_ft_stack_pop(void)
 {
 #if defined(FT_STACK_POP) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_list  l2 = {"2", 1, NULL};
-    t_list  l1 = {"1", 1, &l2};
-    t_stack s = {&l1};
-    is(ft_stack_pop(&s)->content, "1");
-    is(ft_stack_pop(&s)->content, "2");
-    ok(!s.top);
+	t_list  l2 = {"2", 1, NULL};
+	t_list  l1 = {"1", 1, &l2};
+	t_stack s = {&l1};
+	is(ft_stack_pop(&s)->content, "1");
+	is(ft_stack_pop(&s)->content, "2");
+	ok(!s.top);
 #endif
 }
 void	test_ft_stack_push(void)
 {
 #if defined(FT_STACK_PUSH) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_list  l4 = {"4", 1, NULL};
-    t_list  l3 = {"3", 1, NULL};
-    t_list  l2 = {"2", 1, NULL};
-    t_list  l1 = {"1", 1, NULL};
-    t_stack s = {NULL};
-    ft_stack_push(&s, &l1);
-    ft_stack_push(&s, &l2);
-    ft_stack_push(&s, &l3);
-    ft_stack_push(&s, &l4);
-    is(s.top->content, "4");
-    is(s.top->next->content, "3");
+	t_list  l4 = {"4", 1, NULL};
+	t_list  l3 = {"3", 1, NULL};
+	t_list  l2 = {"2", 1, NULL};
+	t_list  l1 = {"1", 1, NULL};
+	t_stack s = {NULL};
+	ft_stack_push(&s, &l1);
+	ft_stack_push(&s, &l2);
+	ft_stack_push(&s, &l3);
+	ft_stack_push(&s, &l4);
+	is(s.top->content, "4");
+	is(s.top->next->content, "3");
 #endif
 }
 void	test_ft_queue_new(void)
 {
 #if defined(FT_QUEUE_NEW) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_queue *q = ft_queue_new();
-    ok(!q->front);
-    ok(!q->rear);
-    free(q);
+	t_queue *q = ft_queue_new();
+	ok(!q->front);
+	ok(!q->rear);
+	free(q);
 #endif
 }
 void	test_ft_dequeue(void)
 {
 #if defined(FT_DEQUEUE) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_list  l2 = {"2", 1, NULL};
-    t_list  l1 = {"1", 1, &l2};
-    t_queue q = {&l1, &l2};
-    t_list  dq = *ft_dequeue(&q);
-    is(dq.content, "1");
-    is(q.rear->content, "2");
-    is(q.front->content, "2");
-    dq = *ft_dequeue(&q);
-    is(dq.content, "2");
-    ok(q.rear == NULL);
-    ok(q.front == NULL);
+	t_list  l2 = {"2", 1, NULL};
+	t_list  l1 = {"1", 1, &l2};
+	t_queue q = {&l1, &l2};
+	t_list  dq = *ft_dequeue(&q);
+	is(dq.content, "1");
+	is(q.rear->content, "2");
+	is(q.front->content, "2");
+	dq = *ft_dequeue(&q);
+	is(dq.content, "2");
+	ok(q.rear == NULL);
+	ok(q.front == NULL);
 #endif
 }
 void	test_ft_enqueue(void)
 {
 #if defined(FT_ENQUEUE) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_list  l4 = {"4", 1, NULL};
-    t_list  l3 = {"3", 1, NULL};
-    t_list  l2 = {"2", 1, NULL};
-    t_list  l1 = {"1", 1, NULL};
-    t_queue q = {NULL, NULL};
-    ft_enqueue(&q, &l1);
-    ft_enqueue(&q, &l2);
-    ft_enqueue(&q, &l3);
-    ft_enqueue(&q, &l4);
-    is(ft_dequeue(&q)->content, "1");
-    is(ft_dequeue(&q)->content, "2");
-    is(ft_dequeue(&q)->content, "3");
-    is(ft_dequeue(&q)->content, "4");
-    ok(q.front == NULL);
-    ok(q.rear == NULL);
+	t_list  l4 = {"4", 1, NULL};
+	t_list  l3 = {"3", 1, NULL};
+	t_list  l2 = {"2", 1, NULL};
+	t_list  l1 = {"1", 1, NULL};
+	t_queue q = {NULL, NULL};
+	ft_enqueue(&q, &l1);
+	ft_enqueue(&q, &l2);
+	ft_enqueue(&q, &l3);
+	ft_enqueue(&q, &l4);
+	is(ft_dequeue(&q)->content, "1");
+	is(ft_dequeue(&q)->content, "2");
+	is(ft_dequeue(&q)->content, "3");
+	is(ft_dequeue(&q)->content, "4");
+	ok(q.front == NULL);
+	ok(q.rear == NULL);
 #endif
 }
 void	test_ft_queue_del(void)
 {
 #if defined(FT_QUEUE_DEL) || defined(FT_ALL) || defined(FT_EXTRA2)
-    t_queue *q;
-    q = malloc(sizeof(*q));
-    t_list  *l;
+	t_queue *q;
+	q = malloc(sizeof(*q));
+	t_list  *l;
 	l = malloc(sizeof(*l));
-    char c[] = ".....";
-    *l = (t_list){c, 4, NULL};
-    q->rear = l;
-    q->front = l;
-    ft_queue_del(&q, stack_del);
-    ok(q == NULL);
-    is(c, "!!!!.");
+	char c[] = ".....";
+	*l = (t_list){c, 4, NULL};
+	q->rear = l;
+	q->front = l;
+	ft_queue_del(&q, stack_del);
+	ok(q == NULL);
+	is(c, "!!!!.");
 #endif
 }
 void	test_ft_print_bits_fd(void)
@@ -533,80 +607,80 @@ void	test_ft_htdel(void)
 }
 int		main()
 {
-    test_ft_memset();
-    test_ft_bzero();
-    test_ft_memcpy();
-    test_ft_memccpy();
-    test_ft_memmove();
-    test_ft_memchr();
-    test_ft_memcmp();
-    test_ft_strlen();
-    test_ft_strdup();
-    test_ft_strcpy();
-    test_ft_strncpy();
-    test_ft_strcat();
-    test_ft_strncat();
-    test_ft_strlcat();
-    test_ft_strchr();
-    test_ft_strrchr();
-    test_ft_strstr();
-    test_ft_strnstr();
-    test_ft_strcmp();
-    test_ft_strncmp();
-    test_ft_atoi();
-    test_ft_isalpha();
-    test_ft_isdigit();
-    test_ft_isalnum();
-    test_ft_isascii();
-    test_ft_isprint();
-    test_ft_toupper();
-    test_ft_tolower();
-    test_ft_memalloc();
-    test_ft_memdel();
-    test_ft_strnew();
-    test_ft_strdel();
-    test_ft_strclr();
-    test_ft_striter();
-    test_ft_striteri();
-    test_ft_strmap();
-    test_ft_strmapi();
-    test_ft_strequ();
-    test_ft_strnequ();
-    test_ft_strsub();
-    test_ft_strjoin();
-    test_ft_strtrim();
-    test_ft_strsplit();
-    test_ft_itoa();
-    test_ft_putchar();
-    test_ft_putstr();
-    test_ft_putendl();
-    test_ft_putnbr();
-    test_ft_putchar_fd();
-    test_ft_putstr_fd();
-    test_ft_putendl_fd();
-    test_ft_putnbr_fd();
-    test_ft_lstnew();
-    test_ft_lstdelone();
-    test_ft_lstdel();
-    test_ft_lstadd();
-    test_ft_lstiter();
-    test_ft_lstmap();
-    test_ft_count_till();
-    test_ft_iswhitespace();
-    test_ft_lstreverse();
-    test_ft_stack_new();
-    test_ft_stack_del();
-    test_ft_stack_pop();
-    test_ft_stack_push();
-    test_ft_queue_new();
-    test_ft_dequeue();
-    test_ft_enqueue();
-    test_ft_queue_del();
+	test_ft_memset();
+	test_ft_bzero();
+	test_ft_memcpy();
+	test_ft_memccpy();
+	test_ft_memmove();
+	test_ft_memchr();
+	test_ft_memcmp();
+	test_ft_strlen();
+	test_ft_strdup();
+	test_ft_strcpy();
+	test_ft_strncpy();
+	test_ft_strcat();
+	test_ft_strncat();
+	test_ft_strlcat();
+	test_ft_strchr();
+	test_ft_strrchr();
+	test_ft_strstr();
+	test_ft_strnstr();
+	test_ft_strcmp();
+	test_ft_strncmp();
+	test_ft_atoi();
+	test_ft_isalpha();
+	test_ft_isdigit();
+	test_ft_isalnum();
+	test_ft_isascii();
+	test_ft_isprint();
+	test_ft_toupper();
+	test_ft_tolower();
+	test_ft_memalloc();
+	test_ft_memdel();
+	test_ft_strnew();
+	test_ft_strdel();
+	test_ft_strclr();
+	test_ft_striter();
+	test_ft_striteri();
+	test_ft_strmap();
+	test_ft_strmapi();
+	test_ft_strequ();
+	test_ft_strnequ();
+	test_ft_strsub();
+	test_ft_strjoin();
+	test_ft_strtrim();
+	test_ft_strsplit();
+	test_ft_itoa();
+	test_ft_putchar();
+	test_ft_putstr();
+	test_ft_putendl();
+	test_ft_putnbr();
+	test_ft_putchar_fd();
+	test_ft_putstr_fd();
+	test_ft_putendl_fd();
+	test_ft_putnbr_fd();
+	test_ft_lstnew();
+	test_ft_lstdelone();
+	test_ft_lstdel();
+	test_ft_lstadd();
+	test_ft_lstiter();
+	test_ft_lstmap();
+	test_ft_count_till();
+	test_ft_iswhitespace();
+	test_ft_lstreverse();
+	test_ft_stack_new();
+	test_ft_stack_del();
+	test_ft_stack_pop();
+	test_ft_stack_push();
+	test_ft_queue_new();
+	test_ft_dequeue();
+	test_ft_enqueue();
+	test_ft_queue_del();
 	test_ft_print_bits_fd();
-    test_ft_itoa_base();
-    test_ft_htnew();
-    test_ft_htset();
-    test_ft_htget();
-    test_ft_htdel();
+	test_ft_itoa_base();
+	test_ft_htnew();
+	test_ft_htset();
+	test_ft_htget();
+	test_ft_htdel();
 	return (0);
 }
