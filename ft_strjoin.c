7cfa65e0735a9ecc402a747f1901c11b2f865249
Michail Pasechnik <michail.goodchild@gmail.com>
2019-05-07 18:26:30 +0300

    ft_strjoin.c - fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -17,10 +17,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*new;
 	size_t	len;
 
-	len = ft_strlen(s1) + ft_strlen(s2) - 1;
+	len = ft_strlen(s1) + ft_strlen(s2);
 	if ((new = ft_strnew(len)) == NULL)
 		return (NULL);
-	ft_memset(new, '\0', len + 1);
+	ft_memset(new, '\0', len);
 	ft_strcat(new, s1);
 	ft_strcat(new, s2);
 	return (new);
