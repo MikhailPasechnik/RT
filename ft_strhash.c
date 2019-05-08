cdaf477b49d2f30fdd44796506be60a8eaed390d
Brooke Nesoi <bnesoi@oa-l6.21-school.ru>
2019-05-08 20:03:44 +0300

    norm

--- ft_strhash.c
+++ ft_strhash.c
@@ -6,7 +6,7 @@
 /*   By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 13:05:44 by bnesoi            #+#    #+#             */
-/*   Updated: 2019/04/13 13:54:41 by bnesoi           ###   ########.fr       */
+/*   Updated: 2019/05/08 20:03:07 by bnesoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 size_t	ft_strhash(const char *s, size_t size)
 {
-	size_t  hash;
-	size_t  i;
+	size_t	hash;
+	size_t	i;
 
 	i = 0;
 	hash = 0;
-	while(s && s[i])
+	while (s && s[i])
 	{
 		hash = (hash + s[i]) % size;
 		i++;
 	}
-	return hash;
+	return (hash);
 }
