bd466df9f5cd85feba8c93939309af139415d8c2
Brooke Nesoi <bnesoi@oa-l6.21-school.ru>
2019-05-08 20:08:18 +0300

    unised_fix

--- tests/test_ft_lstnew.c
+++ tests/test_ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 09:23:34 by bnesoi            #+#    #+#             */
-/*   Updated: 2019/04/26 10:21:57 by bnesoi           ###   ########.fr       */
+/*   Updated: 2019/05/08 20:07:07 by bnesoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 static void		del(void *c, size_t s)
 {
+	(void)c;
+	(void)s;
 }
 
 int				main(void)
