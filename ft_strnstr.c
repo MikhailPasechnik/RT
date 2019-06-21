5ff192c9e29f660e8e2628af80277f5b14d6de35
Michail Pasechnik <michail.goodchild@gmail.com>
2019-06-21 15:15:21 +0300

    ft_strnstr.c - fix return

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,8 +18,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t		j;
 	size_t		i;
 
-	if (!len)
-		return ((char *)haystack);
+	if (ft_strlen(haystack))
+		return (NULL);
 	i = 0;
 	while (*haystack && i < len)
 	{
