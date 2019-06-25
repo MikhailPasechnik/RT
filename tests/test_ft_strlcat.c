07a76d89391f7e5d9f13f50cbb350693df7156a9
Michail Pasechnik <michail.goodchild@gmail.com>
2019-06-25 14:00:02 +0300

    ft_strlcat.c test

--- tests/test_ft_strlcat.c
+++ tests/test_ft_strlcat.c
@@ -22,5 +22,10 @@ int		main(void)
 	FT_CMP(ft_strlcat(d, "bc", 3), 4, "==");
 	FT_CMP(ft_strlcat(d, "bcd", 3), 5, "==");
 	ft_strdel(&d);
+	char *d2 = "the cake is a lie !\0I'm hidden lol";
+	char b1[0xF00] = "there is no stars in the sky";
+	char b2[0xF00] = "there is no stars in the sky";
+	ft_strlcat(b2, d2, ft_strlen(d2) + ft_strlen(b2));
+	FT_IS(b2, "there is no stars in the skythe cake is a lie ");
 	return (0);
 }
