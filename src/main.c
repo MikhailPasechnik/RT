a46ba8c2c328390bc9bf3d02fcf6ba8984d7f2ab
michail <michail.goodchild@gmail.com>
2019-07-15 22:15:23 +0300

    to tabs

--- src/main.c
+++ src/main.c
@@ -14,23 +14,23 @@
 
 void    init_mlx(t_mlx *mlx)
 {
-    mlx->mlx_ptr = mlx_init();
-    mlx->size_x = WIN_X;
-    mlx->size_y = WIN_Y;
-    mlx->win_ptr = mlx_new_window(
-            mlx->mlx_ptr, mlx->size_x, mlx->size_y, WIN_TITLE);
-    mlx->img_ptr = mlx_new_image(mlx->mlx_ptr, mlx->size_x, mlx->size_y);
-    mlx->pic_ptr = (int *)mlx_get_data_addr(
-            mlx->img_ptr, &mlx->bpp, &mlx->size_line, &mlx->endian);
-    mlx_key_hook(mlx->win_ptr,  &key_press, mlx);
-    mlx_hook(mlx->win_ptr, 17, 0, &finish, mlx);
+	mlx->mlx_ptr = mlx_init();
+	mlx->size_x = WIN_X;
+	mlx->size_y = WIN_Y;
+	mlx->win_ptr = mlx_new_window(
+			mlx->mlx_ptr, mlx->size_x, mlx->size_y, WIN_TITLE);
+	mlx->img_ptr = mlx_new_image(mlx->mlx_ptr, mlx->size_x, mlx->size_y);
+	mlx->pic_ptr = (int *)mlx_get_data_addr(
+			mlx->img_ptr, &mlx->bpp, &mlx->size_line, &mlx->endian);
+	mlx_key_hook(mlx->win_ptr,  &key_press, mlx);
+	mlx_hook(mlx->win_ptr, 17, 0, &finish, mlx);
 }
 
 void    start()
 {
-    t_mlx mlx;
-    init_mlx(&mlx);
-    mlx_loop(mlx.mlx_ptr);
+	t_mlx mlx;
+	init_mlx(&mlx);
+	mlx_loop(mlx.mlx_ptr);
 }
 
 int		main(int argc, char **argv)
@@ -40,6 +40,6 @@ int		main(int argc, char **argv)
 		ft_putstr(USG_MSG);
 		return (0);
 	}
-    start();
+	start();
 	return (0);
 }
