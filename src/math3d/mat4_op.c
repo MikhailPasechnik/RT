069113001e45a0aaf7af562a78a1cf30ac8e86e0
bnesoi <bnesoi@student.21-school.ru>
2020-03-17 10:41:39 +0300

    remove printf

--- src/math3d/mat4_op.c
+++ src/math3d/mat4_op.c
@@ -22,10 +22,6 @@ void	m4_rotate_relative(t_mat4 *m, t_mat4 *r, t_vec3 *rot)
 	m4_inv(&ir, &im);
 	m4_mul(&im, r, r);
 	m4_mul(r, m, m);
-//	m4_mul(p, &inv_m, p);
-//	m4_mul(m, p, m);
-//	m4_rotate(p, rot);
-//	m4_mul(m, p, m);
 }
 /*
 ** M.s0, M.s1, M.s2, M.s3
@@ -41,7 +37,6 @@ t_mat4	look_at(t_vec3 from, t_vec3 to)
 	t_vec3 right;
 	t_vec3 up;
 
-	printf("from=====%f %f %f=====\n", from.x, from.y, from.z);
 	v3_norm(v3_sub(&from, &to, &forward), &forward);
 	v3_cross(&VEC(0, 0, 1), &forward, &right);
 	v3_cross(&forward, &right, &up);
