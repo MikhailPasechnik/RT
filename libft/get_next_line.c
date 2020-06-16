e868e271856f8a77eefdbf6a59d8c76484d4e1da
Ekaterina Gridneva <ktgri@MacBook-Air-Ekaterina.local>
2020-06-16 15:21:26 +0300

    0/0 leaks

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,86 +6,63 @@
 /*   By: ktgri <ktgri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 14:41:19 by bmahi             #+#    #+#             */
-/*   Updated: 2020/06/14 18:31:44 by ktgri            ###   ########.fr       */
+/*   Updated: 2020/06/16 15:20:51 by ktgri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-# ifndef OPEN_MAX
-# define OPEN_MAX 265
-# endif
-
 # define BUFF_SIZE 30
 
-static int	gnl_check_line(char **s, char **line)
+static int	gnl_read_line(int fd, int ret, char **s, char **line)
 {
-	int		i;
-	char	*stack;
-	char	*tmp_stack;
+	char	*tmp;
+	int		len;
 
-	i = 0;
-	stack = *s;
-	while (stack[i] != '\n')
-		if (!stack[i++])
-			return (0);
-	tmp_stack = &stack[i];
-	*tmp_stack = '\0';
-	*line = ft_strdup(*s);
-	*s = ft_strdup(tmp_stack + 1);
-	free(stack);
+	len = 0;
+	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+		len++;
+	if (s[fd][len] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, len);
+		tmp = ft_strdup(s[fd] + len + 1);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
+	else if (s[fd][len] == '\0')
+	{
+		if (ret == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
+	}
 	return (1);
 }
 
-static int	gnl_read_fd(int fd, char *pec, char **s, char **line)
+int	get_next_line(const int fd, char **line)
 {
-	int		ret_value;
-	char	*tmp_stack;
+	int			ret_value;
+	static char	*s[OPEN_MAX];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
 
-	while ((ret_value = read(fd, pec, BUFF_SIZE)) > 0)
+	if ((fd < 0 || fd >= OPEN_MAX) || !line || (read(fd, s[fd], 0) < 0))
+		return (-1);
+	while ((ret_value = read(fd, buf, BUFF_SIZE)))
 	{
-		pec[ret_value] = '\0';
-		if (*s)
-		{
-			tmp_stack = *s;
-			*s = ft_strjoin(tmp_stack, pec);
-			free(tmp_stack);
-			tmp_stack = NULL;
-		}
-		else
-			*s = ft_strdup(pec);
-		if (gnl_check_line(s, line))
+		buf[ret_value] = '\0';
+		if (!s[fd])
+			s[fd] = ft_strnew(1);
+		tmp = ft_strjoin(s[fd], buf);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	if (ret_value > 0)
-		return (1);
-	return (ret_value);
-}
-
-int			get_next_line(const int fd, char **line)
-{
-	int			i;
-	int			ret_value;
-	char		*pec;
-	static char	*s[OPEN_MAX];
-
-	i = 0;
-	pec = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-	if (!line || (fd < 0 || fd >= OPEN_MAX) || (read(fd, s[fd], 0) < 0) || !pec)
+	if (ret_value < 0)
 		return (-1);
-	if (s[fd])
-		if (gnl_check_line(&s[fd], line))
-			return (1);
-	while (i < BUFF_SIZE)
-		pec[i++] = '\0';
-	ret_value = gnl_read_fd(fd, pec, &s[fd], line);
-	free(pec);
-	if (ret_value != 0 || s[fd] == NULL || s[fd][0] == '\0')
-	{
-		if (!ret_value && *line)
-			*line = NULL;
-		return (ret_value);
-	}
-	*line = s[fd];
-	s[fd] = NULL;
-	return (1);
+	else if (!ret_value && (!s[fd] || s[fd][0] == '\0'))
+		return (0);
+	return (gnl_read_line(fd, ret_value, s, line));
 }
