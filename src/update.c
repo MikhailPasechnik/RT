8e3bf6f7c700df9710a88e98837c0dab1892329d
MikhailPasechnik <michail.goodchild@gmail.com>
2020-08-06 12:44:03 +0300

    revert CMakeLists.txt and remove typo

--- src/update.c
+++ src/update.c
@@ -20,7 +20,6 @@ int			update_object(t_app *app, int index, t_obj *obj)
 	{
 		buff = app->ren.obj_buf.host;
 		buff[index] = *obj;
-		buff->i = obj->i;
 	}
 	return (push_buffer(app->ren.queue, &app->ren.obj_buf,
 						sizeof(t_obj), sizeof(t_obj) * index));
